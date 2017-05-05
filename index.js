ffc4841cad8394bde9dc395082fbd5e811d330ec
Kamal Reddy <kamal.is.reddy@gmail.com>
2017-05-05 21:03:59 +0530

    reduced concurrent promises to 10

--- index.js
+++ index.js
@@ -1312,7 +1312,7 @@ class ArchaeInstaller {
                 .then(() => _install(module, moduleName))
                 .then(() => _build(module, moduleName));
             // }, 20)
-              }, {concurrency: 5})
+              }, {concurrency: 10})
       // }))
         .then(() => {
           cb();
